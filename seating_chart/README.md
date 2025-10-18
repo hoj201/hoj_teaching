40ca87d5029006858e96ef8cf8e9471f7f6d0e26
Henry O. Jacobs <hoj201@gmail.com>
2025-10-18 10:08:45 -0500

    Seat chart (#2)
    * added seating chart module (still ugly)
    
    * added preferential seating
    
    * added other rosters
    
    * added rosters
    
    * added donows
    
    * added todo-list
    
    * use date as seed for random number generator
    
    * fill smallest table first
    
    * added ability for students to avoid one student

---
+++ seating_chart/README.md
@@ -0,0 +1,16 @@
+# Description
+Code for generating welcome slides
+
+```shell
+poetry run python make_slides.py
+```
+
+You can customize the slides by editing
+ - `agenda.json`
+ - `announcements.json`
+ - `do_nows.json`
+
+# Todo
+ - allow use of url-links in agenda
+ - post slides to s3-bucket so they can be viewed as a web-page
+ - make streamlet interface so you can run code from anywhere
---
+++ seating_chart/seating_chart/slide.py
@@ -0,0 +1,124 @@
+from .seats import Table
+from typing import List
+
+import xml.etree.ElementTree as ET
+
+
+WIDTH = 1920
+HEIGHT = 1080
+HEADER_SIZE = "54"
+FONT_SIZE = "44"
+FONT_FAMILY = "Helvetica"
+BG_COLOR = "#13005e"
+BULLET = "\u2022"
+
+def make_slides(tables: List[Table], announcements: List[str], agenda: List[str], donows: List[str]) -> ET.Element:    
+    # Create the root <svg> element with its attributes
+    svg_attributes = {
+        "width": str(WIDTH),
+        "height": str(HEIGHT),
+        "xmlns": "http://www.w3.org/2000/svg"
+    }
+    svg = ET.Element('svg', svg_attributes)
+    ET.SubElement(svg, 'rect', attrib={
+        "width": "100%",
+        "height": "100%",
+        "fill": BG_COLOR
+    })
+    insert_tables(tables, svg)
+    insert_agenda(agenda, svg)
+    insert_announcements(announcements, svg)
+    insert_donows(donows, svg)
+    return svg
+
+def insert_tables(tables: List[Table], svg: ET.Element):
+    width = int(svg.attrib["width"])
+    dx = width // 5
+    if len(tables) > 9:
+        raise ValueError("Slide code assumes fewer than 9 tables")
+    for index, table in enumerate(tables):
+        offset_x = (index % 3) * dx + dx//2
+        offset_y = (index // 3) * dx + dx//2
+        # Create the <circle> element as a sub-element of <svg>
+        circle = ET.SubElement(svg, 'circle', {
+            "cx": str(offset_x),
+            "cy": str(offset_y),
+            "r": str(dx//2),
+            "fill": "#5C0500",
+            "stroke": "black",
+            "stroke-width": "4"
+        })
+
+        # Create the <text> element and add it to <svg>
+        t1 = ET.SubElement(svg, 'text', {
+            "x": str(offset_x),
+            "y": str(offset_y - 50),
+            "font-family": FONT_FAMILY,
+            "font-size": HEADER_SIZE,
+            "fill": "white",
+            "text-anchor": "middle",
+        })
+        t1.text = f"Table {index+1}" # Set the text content inside the tag
+        for student in table:
+            t1_student = ET.SubElement(t1, 'tspan', {
+                "x": str(offset_x),
+                "dy": "1.2em",
+                "font-size": FONT_SIZE,
+            })
+            t1_student.text = student.name
+
+def insert_agenda(agenda_items: List[str], svg: ET.Element):
+    width = int(svg.attrib["width"])
+    height = int(svg.attrib["height"])
+    agenda_x = width-1.9*width//5
+    agenda_y = height //10
+    agenda = ET.SubElement(svg, 'text', {
+        "x": str(agenda_x),
+        "y": str(agenda_y),
+        "font-family": FONT_FAMILY,
+        "font-size": HEADER_SIZE,
+        "fill": "white",
+        "text-anchor": "left",
+    })
+    agenda.text = "Agenda"
+    insert_tspan_list(agenda, agenda_items)
+
+def insert_announcements(announcements: List[str], svg: ET.Element):
+    width = int(svg.attrib["width"])
+    height = int(svg.attrib["height"])
+    x = width-1.9*width//5
+    ann_elem = ET.SubElement(svg, 'text', {
+        "x": str(x),
+        "y": str(height//3 + 50),
+        "font-family": FONT_FAMILY,
+        "font-size": HEADER_SIZE,
+        "fill": "white",
+        "text-anchor": "left",
+    })
+    ann_elem.text = "Announcements"
+    insert_tspan_list(ann_elem, announcements)
+
+
+def insert_donows(items: List[str], svg: ET.Element):
+    width = int(svg.attrib["width"])
+    height = int(svg.attrib["height"])
+    x = width-1.9*width//5
+    donow = ET.SubElement(svg, 'text', {
+        "x": str(x),
+        "y": str(2*height//3),
+        "font-family": FONT_FAMILY,
+        "font-size": HEADER_SIZE,
+        "fill": "white",
+        "text-anchor": "left",
+    })
+    donow.text = "Do Now"
+    insert_tspan_list(donow, items)
+
+def insert_tspan_list(parent: ET.Element, items: List[str]):
+    for item in items:
+        element = ET.SubElement(parent, 'tspan', {
+            "dy": "1.2em",
+            "x": parent.attrib["x"],
+            "font-size": FONT_SIZE,
+        })
+        element.text = BULLET + " " + item
