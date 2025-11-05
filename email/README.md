f76efc5b90b1da62fc69008a0b9b39a54166f66e
hoj201 <hoj201@gmail.com>
2025-11-04 22:59:29 -0600

    added original json file that allows us to easily fetch student contact info

---
+++ email/README.md
@@ -0,0 +1,41 @@
+# Description
+An app for sending mass emails to my class
+
+Desired Features:
+ - Toggle for including parents
+ - Email templates to insert student name, parent name, section, in appropriate places
+ - Automatically default to BCC when multiple families are involved.
+ - Select students by section and then individually
+ - Select all students in section toggle
+ - Prohibit sending one-off emails
+ - Review emails before sending
+
+
+ TODO:
+ - [ ] Get student emails in csv with names
+ - [ ] Put email data in the json structure
+ ```json
+{
+    "lastname": "Ochoa",
+    "firstname": "Alex",
+    "email": "27abcdefg@district44.com",
+    "section": "34",
+    "pronouns": "she/her"
+    "guardians": [
+        {
+            "title":"Mr.",
+            "firstname": "Allan",
+            "lastname": "Ochoa",
+            "relation": "Father",
+            "email": "adfads@gmail.com"
+        },
+        {
+            "title":"Mrs.",
+            "firstname": "Mallory",
+            "lastname": "Ochoa",
+            "relation": "Mother",
+            "email": "asdfasdf@yahoo.com"
+        }
+    ]
+}
+ ```
---
+++ email/process_email_files.py
@@ -0,0 +1,51 @@
+from csv import DictReader
+import re
+
+with open("./fixtures/studentEmails.txt", "r") as f:
+    doc = f.read()
+
+student_email_addresses = [x.strip() for x in doc.split(";")]
+
+students = []
+with open("./fixtures/parentEmails.csv", "r") as f:
+    reader = DictReader(f, fieldnames=["Name", "Contact Name", "Contact Email", "Course"], quotechar='"', delimiter=";")
+    for row in reader:
+        if row["Name"] != "":
+            lastname, firstname = row["Name"].split(",")
+            student = {
+                "firstname": firstname.strip().replace('"', ''),
+                "lastname": lastname.strip().replace('"', ''),
+                "contacts": [],
+                "course": row["Course"]
+            }
+            students.append(student)
+        else:
+            lastname = re.search(r"([^,]+),", row["Contact Name"]).group(1).strip()
+            title = re.search(r"^([^\s]+)", lastname).group(1)
+            if title is not None:
+                lastname = lastname.removeprefix(title).strip() 
+            contact = {
+                "relation" : re.search(r"\(([^)]*)\)", row["Contact Name"]).group(1).lower(),
+                "title": title,
+                "firstname": re.search(r", (.*?) \(", row["Contact Name"]).group(1),
+                "lastname": lastname,
+                "contact_name": row["Contact Name"],
+                "contact_email": row["Contact Email"].removesuffix(" (Current - Primary)").strip().lower()
+            }
+            student["contacts"].append(contact)
+
+
+
+for student in students:
+    lastname = student["lastname"].lower().strip()
+    student_email = [x for x in student_email_addresses if lastname in x]
+    if student_email:
+        student_email = student_email[0]
+    else:
+        student_email = None
+    student["email"] = student_email
+    print(student_email)
+
+with open("students.json", "w") as f:
+    import json
+    json.dump(students, f, indent=4)
